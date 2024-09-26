b6dfe9c1a4c0b8d4962c0045e956fcf6218000a8
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-09-25 22:24:33 -0400

    major changes on the client side

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
-<<<<<<< HEAD:She-Net-Tech/client/src/main.jsx
 import WelcomePage from './pages/WelcomePage';  // Import the WelcomePage
 import Signup from './pages/Signup.jsx'
 import SignIn from './pages/signin.jsx';
@@ -15,15 +14,6 @@ import JobsPage from './pages/Jobs';
 // import Login from './pages/Login';  // Assuming Login is already defined
 // import Signup from './pages/Signup';  // Assuming Signup is already defined
 
-=======
-import Home from './pages/Home.jsx';
-import Detail from './pages/Detail.js';
-import NoMatch from './pages/NoMatch.js';
-import Login from './pages/Login.js';
-import Signup from './pages/Signup.js';
-import Success from './pages/Success.js';
-import OrderHistory from './pages/OrderHistory.js';
->>>>>>> 0aa0a8f85606f7ad263516062c00de7572ef9b91:client/src/main.jsx
 
 const router = createBrowserRouter([
   {
